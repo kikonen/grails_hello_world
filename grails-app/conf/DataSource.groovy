a13ab1906866d02c1652bc3498aa9577bb7250cf
kari <mr.kari.ikonen@gmail.com>
2015-05-14 22:10:25 +0300

    Use postgresql for development

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -19,7 +19,11 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            //            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            logSql = true
+            driverClassName = "org.postgresql.Driver"
+            dialect = org.hibernate.dialect.PostgreSQLDialect
+            url = "jdbc:postgresql://127.0.0.1:5432/grails_hello"
         }
     }
     test {
